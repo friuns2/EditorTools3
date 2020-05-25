ecdf93809df6b4553bd6b63b2a4328a0dfdcce9e
friuns2 <dorumonstr@gmail.com>
2020-05-26 01:21:19 +0300

    z

---
+++ Assets/sdk/Editor/BuildToolsSDK.cs
@@ -0,0 +1,45 @@
+using System.IO;
+using Microsoft.Win32;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public partial class BuildToolsSDK : EditorWindow
+{
+
+    
+    
+    [MenuItem("Tools/BuildLevel PC", false)]
+    public static void BuildLevel()
+    {
+        BuildLevel(BuildTarget.StandaloneWindows);
+    }
+    
+    [MenuItem("Tools/BuildLevel Android", false)]
+    public static void BuildLevel2()
+    {
+        BuildLevel(BuildTarget.Android);
+    }
+    public static void BuildLevel(BuildTarget target)
+    {
+        var d = SceneManager.GetActiveScene();
+        EditorSceneManager.SaveScene(d);
+        
+        var assetBundleName = d.name + ".unity3dlevel" + target;
+        var build = new AssetBundleBuild() {assetNames = new[] {d.path}, assetBundleName = assetBundleName};
+        var bundlesDir = "AssetBundles";
+        Directory.CreateDirectory(bundlesDir);
+        // File.Delete("AssetBundles/"+assetBundleName);
+        // File.Delete("AssetBundles/"+assetBundleName+".manifest");
+        AssetBundleManifest man = BuildPipeline.BuildAssetBundles(bundlesDir, new[] {build}, BuildAssetBundleOptions.ForceRebuildAssetBundle, target);
+        Debug.Log("build success2 " + man.name);
+#if sdk
+        PlayerPrefs.SetString("assetPath", Path.GetFullPath(bundlesDir +"/"+ build.assetBundleName));
+        var key = Registry.CurrentUser.OpenSubKey(@"Software\Unity\UnityEditor\Phaneron\", true);
+        RegistryUtilities.CopyKey(key, "Brutal Strike", Registry.CurrentUser.OpenSubKey(@"Software\Phaneron\", true));
+#endif
+    }
+    
+}
+
---
+++ Assets/sdk/Spawn.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public TeamEnum team = TeamEnum.DeathMatch;
+}
+public enum TeamEnum
+{
+    None,
+    Spectators,
+    DeathMatch,
+    Terrorists,
+    CounterTerrorists, Draw,WaitPls,
+    team1,team2,team3,team4,team5,team6,team7,team8,team9,team10,team11,team12,team13,team14,team15,team16,team17,team18
+}
